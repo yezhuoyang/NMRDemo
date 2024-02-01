5c5df779a9b6413daafb27ecaaf9eafd25c81e30
Zhuoyang Ye <yezhuoyang98@gmail.com>
2024-02-01 10:49:29 -0800

    [Bug] Define the renderer before it is called.

--- public/main.js
+++ public/main.js
@@ -3,17 +3,23 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const controls = new OrbitControls( camera, renderer.domElement );
-const loader = new GLTFLoader();
 
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+
+
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+
+
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
 
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
