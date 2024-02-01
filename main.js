1985f9fef8764695b2a2628047368d8f0ad2fb10
Zhuoyang Ye <yezhuoyang98@gmail.com>
2024-02-01 09:37:31 -0800

    Add import map.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 // JavaScript source code
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
